ec2b5a06f99336ab003bbc25c68d6a096d5a0ad9
DonizeteVida <donizetevida1998@outlook.com>
2025-09-18 00:41:15 +0200

    QRCode can be generated

--- src/routes/send/+page.svelte
+++ src/routes/send/+page.svelte
@@ -1,12 +1,27 @@
 <script lang="ts">
+    import { BrowserQRCodeSvgWriter } from "@zxing/browser";
+
+    const qrCodeWriter = new BrowserQRCodeSvgWriter();
+
     let files: FileList | null | undefined = $state();
+
+    let svg: SVGSVGElement;
 
     $effect(() => {
         if (files) {
-            const file: File = files.item(0)!
-            console.log(file.type)
+            const file = files.item(0);
+            if (file) writeFile(file);
         }
     });
+
+    async function writeFile(file: File) {
+        const el = qrCodeWriter.write(await file.text(), 320, 240);
+        svg.outerHTML = el.outerHTML;
+    }
 </script>
 
-<input bind:files type="file" />
+<div>
+    <input bind:files type="file" style="display: block;" />
+
+    <svg bind:this={svg} />
+</div>
