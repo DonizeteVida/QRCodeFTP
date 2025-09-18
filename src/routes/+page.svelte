bef4e030a38720a5a3045656d5331213462cee81
DonizeteVida <donizetevida1998@outlook.com>
2025-09-18 20:28:03 +0200

    Generate QRCodes from files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<a href="/QRCodeFTP/send">Send file</a>
-<a href="/QRCodeFTP/receive">Receive a file</a>
+<script lang="ts">
+    import { resolve } from "$app/paths";
+</script>
+
+<a class="btn btn-blue" href={resolve("/send")}>Send file</a>
+<a class="btn btn-blue" href={resolve("/receive")}>Receive a file</a>
+
+<style lang="postcss">
+    @reference "tailwindcss";
+
+    .btn {
+        @apply font-bold py-2 px-4;
+    }
+    .btn-blue {
+        @apply bg-blue-500 text-white;
+    }
+    .btn-blue:hover {
+        @apply bg-blue-700;
+    }
+</style>
--- src/routes/send/+page.svelte
+++ src/routes/send/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
     import { BrowserQRCodeSvgWriter } from "@zxing/browser";
 
+    let bufferSize = $state(16);
+
     const qrCodeWriter = new BrowserQRCodeSvgWriter();
 
     let files: FileList | null | undefined = $state();
 
-    let svg: SVGSVGElement;
+    let qrCodeArea: HTMLElement;
 
     $effect(() => {
         if (files) {
@@ -14,14 +16,52 @@
         }
     });
 
+    const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
     async function writeFile(file: File) {
-        const el = qrCodeWriter.write(await file.text(), 320, 240);
-        svg.outerHTML = el.outerHTML;
+        let index = 0;
+        const decoder = new TextDecoder("utf-8");
+        const reader = file.stream().getReader({ mode: "byob" });
+
+        while (true) {
+            const buffer = new Uint8Array(new ArrayBuffer(bufferSize));
+            const data = await reader.read(buffer);
+
+            if (data.done) break;
+
+            const text = `${++index}${decoder.decode(data.value)}`;
+            console.log(text);
+
+            const el = qrCodeWriter.write(text, 320, 240);
+
+            qrCodeArea.removeChild(qrCodeArea.firstChild!);
+
+            qrCodeArea.appendChild(el);
+
+            await delay(200);
+        }
     }
 </script>
 
 <div>
     <input bind:files type="file" style="display: block;" />
 
-    <svg bind:this={svg} />
+    <div>
+        <input
+            min="16"
+            max="1024"
+            step="8"
+            bind:value={bufferSize}
+            type="number"
+        />
+        <input
+            bind:value={bufferSize}
+            step="16"
+            type="range"
+            min="8"
+            max="1024"
+        />
+    </div>
+
+    <div bind:this={qrCodeArea}><p></p></div>
 </div>
